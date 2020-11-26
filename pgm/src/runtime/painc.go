c5e24aeca78555a42d2736cb17db66f8ca7e2a0b
Hank <853160447@qq.com>
2020-11-26 11:52:41 +0800

    throw

--- pgm/src/runtime/painc.go
+++ pgm/src/runtime/painc.go
@@ -2,5 +2,15 @@ package runtime
 
 //go:nosplit
 func throw(s string) {
-
+	// Everything throw does should be recursively nosplit so it
+	// can be called even when it's unsafe to grow the stack.
+	systemstack(func() {
+		print("fatal error: ", s, "\n")
+	})
+	//获取当前g
+	gp :=getg()
+	//将状态更换为异常状态
+	if gp.m.throwing == 0{
+		gp.m.throwing = 1
+	}
 }
--- pgm/src/runtime/stubs.go
+++ pgm/src/runtime/stubs.go
@@ -7,6 +7,27 @@ import "unsafe"
 // that fetch the g directly (from TLS or from the dedicated register).
 func getg()*g
 
+// systemstack runs fn on a system stack.
+// If systemstack is called from the per-OS-thread (g0) stack, or
+// if systemstack is called from the signal handling (gsignal) stack,
+// systemstack calls fn directly and returns.
+// Otherwise, systemstack is being called from the limited stack
+// of an ordinary goroutine. In this case, systemstack switches
+// to the per-OS-thread stack, calls fn, and switches back.
+// It is common to use a func literal as the argument, in order
+// to share inputs and outputs with the code around the call
+// to system stack:
+//
+//	... set up y ...
+//	systemstack(func() {
+//		x = bigcall(y)
+//	})
+//	... use x ...
+//
+//runtime/asm_amd64.s  切换到g0执行栈,相当于系统栈的调用
+//go:noescape
+func systemstack(fn func())
+
 
 // in internal/bytealg/equal_*.s
 //go:noescape
